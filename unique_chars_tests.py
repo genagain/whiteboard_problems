7887bfedafbf75b03a53381504f7e5498ec27abc
genagain <ohta.g@husky.neu.edu>
2016-09-07 12:09:59 -0400

    added question to both tests and implementation

--- unique_chars_tests.py
+++ unique_chars_tests.py
@@ -1,6 +1,8 @@
 import unittest
 import unique_chars
 
+# Implement an algorithm to determin if a string has all unique characters.
+# What if you can not use a additional data structures?
 class TestUniqueChars(unittest.TestCase):
 
   def test_not_unique_with_buffer(self):
@@ -18,7 +20,6 @@ class TestUniqueChars(unittest.TestCase):
   def test_unique_wo_buffer(self):
     test_string = "python"
     self.assertEqual(unique_chars.unique_wo_buffer(test_string), True)
-
 
 if __name__ == '__main__':
   unittest.main()
